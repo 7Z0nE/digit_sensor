f1c1989f43feddc2faf7f4e828ccfe0bf4df7984
boris <boris@robot-learning.de>
2020-11-30 20:17:28 +0100

    Renamed package. Fix catkin build

--- scripts/digit.py
+++ scripts/digit.py
@@ -1,36 +1,42 @@
 #! /usr/bin/env python
 import sys
+import rospy
 from std_msgs.msg import Header
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from digit_interface.digit import Digit
+from digit_interface.digit_handler import DigitHandler
 
 
 if __name__=="__main__":
-    myargv = rospy.myargv(argv=sys.argv)
-    serial_id = myargv[1]
-    resolution = myargv[2]
-    fps = myargv[3]
+    rospy.init_node("digit_sensor")
+    argv = rospy.myargv(argv=sys.argv)
+    #serial_id = rospy.get_param('~serial_id')
+    #resolution = rospy.get_param('~resolution')
+    #fps = rospy.get_param('~fps')
+    serial_id = argv[1]
+    resolution = argv[2]
+    fps = int(argv[3])
 
-    if not len(sys.argv) == 4:
+
+    if not len(argv) == 4:
         raise ValueError("Invalid number of arguments. Should be digit.py id resolution fps")
 
     if resolution == "VGA":
-        if not fps in ["15", "30"]:
-            raise ValueError("Invalid fps {} for resolution {}. Allowed {}".format(resolution, fps, ["15", "30"]))
-    elif resolution == "QVGA"]:
-        if not fps in ["30", "60"]:
-            raise ValueError("Invalid fps {} for resolution {}. Allowed {}".format(resolution, fps, ["30", "60"]))
+        if not fps in [15, 30]:
+            raise ValueError("Invalid fps {} for resolution {}. Allowed {}".format(fps, resolution, [15, 30]))
+    elif resolution == "QVGA":
+        if not fps in [30, 60]:
+            raise ValueError("Invalid fps {} for resolution {}. Allowed {}".format(resolution, fps, [30, 60]))
     else:
         raise ValueError("Unknown resolution {}".format(fps))
 
-    digit = Digit(serial_id)
-    digit.set_resolution(resolution)
-    digit.set_fps(int(sys.argv[2]))
+    digit = Digit(serial_id, rospy.get_name())
+    digit.connect()
+    digit.set_resolution(DigitHandler.STREAMS[resolution])
+    digit.set_fps(fps)
 
-    rospy.init_node("digit-{}".format(serial_id))
-
-    pub = rospy.Publisher("/digit/{}".format(serial_id), Image, queue_size=1)
+    pub = rospy.Publisher(rospy.get_name(), Image, queue_size=1)
 
     rate = rospy.Rate(fps)
 
---
+++ setup.py
@@ -0,0 +1,9 @@
+## DO NOT MANUALLY INVOKE
+from setuptools import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+setup_args = generate_distutils_setup(
+        packages=['digit_interface'],
+        package_dir={'':'digit-interface'})
+
+setup(**setup_args)
